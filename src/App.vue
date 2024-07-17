27ea557e92311dfcd08fce1c548fa89b21060dcc
B4869 <nether.demonic@gmail.com>
2024-07-17 12:00:54 +0700

    thai-version

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,18 @@
     >
       <q-input
         filled
-        v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        v-model="ชื่อ"
+        label="ชื่อของมึง"
+        hint="ชื่อและนามสกุล"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
 
       <q-input
         filled
-        type="number"
-        v-model="age"
-        label="Your age *"
+        type="เลข"
+        v-model="อายุ"
+        label="อายุของมึง"
         lazy-rules
         :rules="[
           val => val !== null && val !== '' || 'Please type your age',
